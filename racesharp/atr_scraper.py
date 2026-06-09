dfe42fe8da9198bed72eb5490878d01069719f87
RaceSharp <rzj8bfvny6@privaterelay.appleid.com>
2026-06-09 17:39:10 +0100

    Update atr_scraper.py

--- racesharp/atr_scraper.py
+++ racesharp/atr_scraper.py
@@ -1,1 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
 
+
+def get_atr_page(url):
+
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    response = requests.get(
+        url,
+        headers=headers,
+        timeout=15
+    )
+
+    return response.text
